2267896abc6a0816a1b51c09ce6daf4fd97859cc
Joonho Kim <joonhok@smartfitnow.com>
2024-04-28 21:20:24 +0900

    ShadowIcon 추가

---
+++ components/ShadowIcon.tsx
@@ -0,0 +1,7 @@
+function ShadowIcon() {
+  return (
+    <span className="w-[24px] h-[24px] bg-gradient-to-tr from-[#096cde] via-[#096cde] via-30% to-[#ddf1ff] rounded-[50%] bg-clip-border bg-scroll bg-origin-padding" />
+  );
+}
+
+export default ShadowIcon;
