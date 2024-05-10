a7adc075750a083b2dc630836322bac6b24575a8
Joonho Kim <joonhok@smartfitnow.com>
2024-05-10 18:15:10 +0900

    AnimateSpin 추가

---
+++ components/AnimateSpin.tsx
@@ -0,0 +1,38 @@
+function AnimateSpin() {
+  return (
+    <svg
+      viewBox="0 0 558 558"
+      width="558"
+      height="558"
+      fill="none"
+      aria-hidden="true"
+      className="animate-spin"
+    >
+      <defs>
+        <linearGradient
+          id=":Rddbqnla:"
+          x1="79"
+          y1="16"
+          x2="105"
+          y2="237"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#13B5C8"></stop>
+          <stop offset="1" stop-color="#13B5C8" stop-opacity="0"></stop>
+        </linearGradient>
+      </defs>
+      <path
+        opacity=".2"
+        d="M1 279C1 125.465 125.465 1 279 1s278 124.465 278 278-124.465 278-278 278S1 432.535 1 279Z"
+        stroke="#13B5C8"
+      ></path>
+      <path
+        d="M1 279C1 125.465 125.465 1 279 1"
+        stroke="url(#:Rddbqnla:)"
+        stroke-linecap="round"
+      ></path>
+    </svg>
+  );
+}
+
+export default AnimateSpin;
