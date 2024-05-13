2b289181bca76c836ff609541add8516ec5cc527
Joonho Kim <joonhok@smartfitnow.com>
2024-05-13 10:54:54 +0900

    delay 유틸 함수 추가(비동기 유닛 테스트 작성)

---
+++ utils/delay.ts
@@ -0,0 +1,20 @@
+/**
+ * 'wait' 밀리초 후에 'func'를 호출합니다.
+ *
+ * @param {Function} func 호출할 함수.
+ * @param {number} wait 지연할 밀리초.
+ * @param {...*} [args] 함수에 전달할 인수.
+ * @returns {number} 타이머 ID.
+ * @example
+ *
+ * delay(text => console.log(text), 1000, 'later')
+ * // => Logs 'later' after one second.
+ */
+function delay(func: Function, wait: number, ...args: any[]) {
+  if (typeof func !== "function") {
+    throw new TypeError("Expected a function");
+  }
+  return setTimeout(func, +wait || 0, ...args);
+}
+
+export default delay;
