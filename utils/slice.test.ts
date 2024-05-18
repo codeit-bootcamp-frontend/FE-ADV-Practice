8024bed78d1fb510e75f360a4d31169c5d007b19
Joonho Kim <joonhok@smartfitnow.com>
2024-05-18 16:11:02 +0900

    slice.test.ts 테스트 완성(실습: 간단한 테스트 작성)

--- utils/slice.test.ts
+++ utils/slice.test.ts
@@ -22,20 +22,39 @@ describe("slice", () => {
   );
 
   it("should work with a negative `start`", () => {
-    // 코드를 완성하세요.
+    expect(slice(array, -1)).toEqual([3]);
   });
 
-  // "should work with a negative `start` <= negative `length`",
-  // it.each([-3, -4, -Infinity]) 로 시작하는 테스트 코드를 작성하세요.
+  it.each([-3, -4, -Infinity])(
+    "should work with a negative `start` <= negative `length`",
+    (start) => {
+      expect(slice(array, start)).toEqual(array);
+    }
+  );
 
   it.each([2, 3])("should work with `start` >= `end`", (start) => {
-    // 코드를 완성하세요.
+    expect(slice(array, start, 2)).toEqual([]);
   });
 
   it("should work with a positive `end`", () => {
-    // 코드를 완성하세요.
+    expect(slice(array, 0, 1)).toEqual([1]);
   });
 
-  // "should work with a `end` >= `length`",
-  // it.each([3, 4, 2 ** 32, Infinity]) 로 시작하는 테스트 코드를 작성하세요.
+  it.each([3, 4, 2 ** 32, Infinity])(
+    "should work with a `end` >= `length`",
+    (end) => {
+      expect(slice(array, 0, end)).toEqual(array);
+    }
+  );
+
+  it("should work with a negative `end`", () => {
+    expect(slice(array, 0, -1)).toEqual([1, 2]);
+  });
+
+  it.each([-3, -4, -Infinity])(
+    "should work with a negative `end` <= negative `length`",
+    (end) => {
+      expect(slice(array, 0, end)).toEqual([]);
+    }
+  );
 });
