1c9d0993c9b74a43977481dbce59451e26d57dcb
Joonho Kim <joonhok@smartfitnow.com>
2024-05-20 17:31:37 +0900

    spring-to-rio 페이지 추가 (react-intersection-observer로 스크롤 트리거 애니메이션 만들기)

---
+++ pages/spring-to-rio.tsx
@@ -0,0 +1,39 @@
+import { useInView } from "react-intersection-observer";
+
+function Title({ children }: { children: React.ReactNode }) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: '-100px 0px',
+  });
+  const styleClass = inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
+  return (
+    <div
+      ref={ref}
+      className={`transition-all ${styleClass}`}
+    >
+      <h2 className="text-6xl ">{children}</h2>
+    </div>
+  );
+}
+
+function Page({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex justify-center items-center h-screen">{children}</div>
+  );
+}
+
+const PAGE_TITLES = ["Don't", "you", "just", "hate", "popups?"];
+
+function ReactObserver() {
+  return (
+    <div className="">
+      {PAGE_TITLES.map((title) => (
+        <Page key={title}>
+          <Title>{title}</Title>
+        </Page>
+      ))}
+    </div>
+  );
+}
+
+export default ReactObserver;
