253843163db33e6440084368a3c1b7a68eb61f44
Joonho Kim <joonhok@smartfitnow.com>
2024-05-27 17:07:55 +0900

    UpperInput.test.jsx 스켈레턴 테스트 코드 추가(라이브러리 기본 API 문법 익히기(1))

---
+++ components/UpperInput.test.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import UpperInput from "./UpperInput";
+
+test("sets the value to the upper version of the value", async () => {
+  // UpperInput 컴포넌트를 렌더링합니다.
+
+  // input 요소를 찾습니다.
+
+  // input 요소에 "stuff"를 입력합니다.
+
+  // input 요소의 값이 대문자로 변환되었는지 확인합니다.
+
+});
