1d35618a8ff6bb8546e7fb89edd1507c94bb68a6
Joonho Kim <joonhok@smartfitnow.com>
2024-05-27 17:08:15 +0900

    UpperInput test 코드 추가(라이브러리 기본 API 문법 익히기(1))

--- components/UpperInput.test.jsx
+++ components/UpperInput.test.jsx
@@ -4,11 +4,13 @@ import UpperInput from "./UpperInput";
 
 test("sets the value to the upper version of the value", async () => {
   // UpperInput 컴포넌트를 렌더링합니다.
+  render(<UpperInput />);
 
   // input 요소를 찾습니다.
-
+  const upperInput = screen.getByLabelText(/upper/i);
   // input 요소에 "stuff"를 입력합니다.
-
+  const text = "stuff";
+  fireEvent.change(upperInput, { target: { value: text } });
   // input 요소의 값이 대문자로 변환되었는지 확인합니다.
-
+  expect(upperInput.value).toEqual(text.toUpperCase());
 });
