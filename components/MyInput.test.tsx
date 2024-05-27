ff68d121cfa36ea711f7e5f398b19f1f23f4cfe2
Joonho Kim <joonhok@smartfitnow.com>
2024-05-27 15:54:59 +0900

    MyInput.test.tsx 테스트 추가(라이브러리 기본 API 문법 익히기)

--- components/MyInput.test.tsx
+++ components/MyInput.test.tsx
@@ -5,18 +5,37 @@ import MyInput from "./MyInput";
 describe("MyInput", () => {
   it("should render correctly", () => {
     // MyInput 컴포넌트를 렌더링합니다.
-    
+    const wrapper = render(<MyInput label="test input" />);
     // wrapper.unmount() 함수를 호출해도 에러가 발생하지 않는지 확인합니다.
-    
+    expect(() => wrapper.unmount()).not.toThrow();
   });
 
-  it("should clear the value and onClear is triggered", () => {
+  it("should clear the value and onClear is triggered", async () => {
     // 필요하다면 jest mock 함수나 ref를 생성합니다.
+    const onClear = jest.fn();
+    const ref = React.createRef<HTMLInputElement>();
+
     // MyInput 컴포넌트를 렌더링합니다.
+    const { getByRole } = render(
+      <MyInput
+        ref={ref}
+        isClearable
+        defaultValue="junior@nextui.org"
+        label="test input"
+        onClear={onClear}
+      />
+    );
 
     // clearButton을 클릭합니다.
+    const clearButton = getByRole("button");
+    expect(clearButton).not.toBeNull();
+    fireEvent.click(clearButton);
 
     // input 요소의 값이 ""인지 확인합니다.
     // onClear 함수가 한 번 호출되었는지 확인합니다.
+    await waitFor(() => {
+      expect(ref.current?.value)?.toBe("");
+      expect(onClear).toHaveBeenCalledTimes(1);
+    });
   });
 });
